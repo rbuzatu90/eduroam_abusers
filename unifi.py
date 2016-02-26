89e44300c7bcf13a67cfe498dd231bc09d7e05e1
Remus Buzatu <r.buzatu90@gmail.com>
2016-02-26 18:48:12 +0200

    Added timeout for urllib

--- unifi.py
+++ unifi.py
@@ -15,20 +15,24 @@ def get_cookie():
 
     cj = CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    response = opener.open(login_url, data)
+    try:
+        response = opener.open(login_url, data, timeout=5)
+    except:
+        print "Networking issues ?"
+        return
     content = response.read()
     return response.headers.get('Set-Cookie')
 
 
 def bytes2h(size):
 
-   if (size == 0):
-       return '0B'
-   size_name = ("b","KB", "MB", "GB", "TB")
-   i = int(math.floor(math.log(size,1024)))
-   p = math.pow(1024,i)
-   s = round(size/p,2)
-   return '%s %s' % (s,size_name[i])
+    if (size == 0):
+        return '0B'
+    size_name = ("b","KB", "MB", "GB", "TB")
+    i = int(math.floor(math.log(size,1024)))
+    p = math.pow(1024,i)
+    s = round(size/p,2)
+    return '%s %s' % (s,size_name[i])
 
 
 def h2bytes(size):
@@ -45,6 +49,8 @@ def h2bytes(size):
 def get_abusers(limit):
 
     my_cookie = get_cookie()
+    if not my_cookie:
+        return
     req2 = urllib2.Request(data_url)
     req2.add_header('Cookie', my_cookie)
     response = urllib2.urlopen(req2)
