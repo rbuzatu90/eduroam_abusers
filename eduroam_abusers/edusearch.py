6b936d3c9b8fb3c2efe5e5ed8763443831a73761
Remus Buzatu <r.buzatu90@gmail.com>
2016-04-27 14:58:53 +0300

    Fixed date on query and others

--- eduroam_abusers/edusearch.py
+++ eduroam_abusers/edusearch.py
@@ -9,18 +9,16 @@ def search_for_mac(days_back, mac):
 
     logging.debug("Searching for MAC %s", mac)
     now = datetime.datetime.now()
-    ago = datetime.timedelta(hours=24)
-    offset = (now - ago).isoformat()
-    search_mac = {"query":{"filtered":{"query":{"bool":{"should":[{"query_string":{"query":"*"}}]}},"filter":{"bool":{"must":[{"range":{"@timestamp":{"from":"%s+03" % (offset, ), "to":"now"}}},{"fquery":{"query":{"query_string":{"query":"tags:(\"eduroam\")"}},"_cache":"true"}},{"fquery":{"query":{"query_string":{"query":"mac:(\"whatever\")"}},"_cache":"true"}}]}}}},"highlight":{"fields":{},"fragment_size":2147483647,"pre_tags":["@start-highlight@"],"post_tags":["@end-highlight@"]},"size":1000,"sort":[{"@timestamp":{"order":"desc","ignore_unmapped":"true"}},{"@timestamp":{"order":"desc","ignore_unmapped":"true"}}]}
+    search_mac = {"query":{"filtered":{"query":{"bool":{"should":[{"query_string":{"query":"*"}}]}},"filter":{"bool":{"must":[{"range":{"@timestamp":{"lt" : "now" }}},{"fquery":{"query":{"query_string":{"query":"tags:(\"eduroam\")"}},"_cache":"true"}},{"fquery":{"query":{"query_string":{"query":"mac:(\"whatever\")"}},"_cache":"true"}}]}}}},"highlight":{"fields":{},"fragment_size":2147483647,"pre_tags":["@start-highlight@"],"post_tags":["@end-highlight@"]},"size":500,"sort":[{"@timestamp":{"order":"desc","ignore_unmapped":"true"}},{"@timestamp":{"order":"desc","ignore_unmapped":"true"}}]}
     new_mac = 'mac:("%s")' % mac
     search_mac['query']['filtered']['filter']['bool']['must'][2]['fquery']['query']['query_string']['query'] = new_mac
     query_json = json.dumps(search_mac)
     users = {}
     for day_ago in range(days_back, -1, -1):
-        logstash_url2 = "%slogstash-%d.%.2d.%.2d/_search?pretty=true" % (logstash_url, now.year, now.month, now.day-day_ago)
-        req = urllib2.Request(logstash_url2, query_json, {'Content-Type': 'application/json'})
+        logstash_url = "%slogstash-%d.%.2d.%.2d/_search?pretty=true" % (logstash_address, now.year, now.month, now.day-day_ago)
+        req = urllib2.Request(logstash_url, query_json, {'Content-Type': 'application/json'})
         try:
-            f = urllib2.urlopen(req, timeout=15)
+            f = urllib2.urlopen(req, timeout=60)
         except:
             logging.critical("Networking issues!")
             return
@@ -31,7 +29,9 @@ def search_for_mac(days_back, mac):
                 users[hit['_source']['user']] = 1
             else:
                 users[hit['_source']['user']] += 1
-            logging.info("Found user {user} associated at {ap}".format(user=hit['_source']['user'], ap=hit['_source']['ap-name']))
+
+            logging.debug("Found user {user} associated at {ap}".format(user=hit['_source']['user'], ap=hit['_source']['ap-name']))
+    print "Found user {user} with {hits} hits when searching for {mac}".format(user=users.keys(), hits=users.values(), mac=mac)
     return users
 
 def search_for_user(days_back, user):
